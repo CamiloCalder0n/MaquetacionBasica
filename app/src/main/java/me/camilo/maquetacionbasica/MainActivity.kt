5a88e7cec1bdbdf0e02fb852e8c64c41391ee084
CamiloCalder0n <jcalderon106@unab.edu.co>
2026-02-27 08:57:52 -0500

    feat:tercera actividad

--- app/src/main/java/me/camilo/maquetacionbasica/MainActivity.kt
+++ app/src/main/java/me/camilo/maquetacionbasica/MainActivity.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -45,6 +46,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Primero()
             Segundo()
+            Tercero()
 
 
 
@@ -386,7 +388,42 @@ fun Segundo() {
         }
     }
 }
+@Composable
+@Preview(showSystemUi = true)
+fun Tercero() {
 
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
 
+        Image(
+            painter = painterResource(id = R.drawable.captura),
+            contentDescription = "Header",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(180.dp)
+        )
+
+        Text(
+            text = "Jetpack Compose tutorial",
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        Text(
+            text = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app's UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI's construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
 
 
